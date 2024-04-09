84a8d77b23fce461afa2c90e6de9c8c2b54bb3de
MendozaNicolas <nicolas.mendozaion@gmail.com>
2024-04-09 08:29:54 -0300

    agregue readme y pequeños cambios

---
+++ README.md
@@ -0,0 +1,45 @@
+# CaPYbara Game
+
+![Preview](/preview.png)
+<br>
+<br>
+
+**Juego creado** en **Python** usando la libreria **Pygame**! 
+
+
+## Tabla de contenidos
+
+- [CaPYbara Game](#capybara-game)
+  - [Tabla de contenidos](#tabla-de-contenidos)
+  - [Descripción](#descripción)
+  - [Instalación](#instalación)
+  - [Controles](#controles)
+  - [Librerias](#librerias)
+
+## Descripción
+
+
+Esta es la implementacion sencilla de un juego de Python utilizando la libreria Pygame. El juego es basico y tenes que ir esquivando enemigos y obstaculos, cuantos mas puntos tengas, mas dificil será.
+
+## Instalación
+Requerimientos: Necesitas tener python instalado
+
+1. Clona el repositorio
+2. Abre la terminal, navega al directorio donde el repositorio fue clonado, e.g., `C:\Users\Nico\pythonProyectos\CaPYbara-Game`
+3. Instala la libreria:
+    ```bash
+    pip install pygame 
+    ```
+4. Ejecuta el juego utilizando el siguiente comando:
+    ```bash
+    python main.py
+    ```
+
+## Controles
+- Usa la flecha arriba para saltar.
+- Usa la flecha abajo para agacharse
+- Presionar cualquier tecla para empezar el juego.
+
+## Librerias
+
+- [pygame](https://www.pygame.org/news): Pygame is a cross-platform set of Python modules designed for writing video games.
--- main.py
+++ main.py
@@ -10,13 +10,13 @@ pygame.init()
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Juego dinosaurio")
+pygame.display.set_caption("Juego carpincho")
 
-# RUNNING = [pygame.image.load(os.path.join("Assets/Dino", "DinoRun1.png")),
-#         pygame.image.load(os.path.join("Assets/Dino", "DinoRun2.png"))]
-# JUMPING = pygame.image.load(os.path.join("Assets/Dino", "DinoJump.png"))
-# DUCKING = [pygame.image.load(os.path.join("Assets/Dino", "DinoDuck1.png")),
-#         pygame.image.load(os.path.join("Assets/Dino", "DinoDuck2.png"))]
+# RUNNING = [pygame.image.load(os.path.join("Assets/capy", "capyRun1.png")),
+#         pygame.image.load(os.path.join("Assets/capy", "capyRun2.png"))]
+# JUMPING = pygame.image.load(os.path.join("Assets/capy", "capyJump.png"))
+# DUCKING = [pygame.image.load(os.path.join("Assets/capy", "capyDuck1.png")),
+#         pygame.image.load(os.path.join("Assets/capy", "capyDuck2.png"))]
 
 RUNNING = [
         pygame.image.load(os.path.join("Assets/Capy", "CapyRun0.png")),
@@ -46,7 +46,7 @@ CLOUD = pygame.image.load(os.path.join("Assets/Other", "Cloud.png"))
 
 BG = pygame.image.load(os.path.join("Assets/Other", "Track.png"))
 
-class Dinosaur:
+class Capybara:
     X_POS = 80
     Y_POS = 311
     Y_POS_DUCK = 350
@@ -57,66 +57,66 @@ class Dinosaur:
         self.run_img = RUNNING
         self.jump_img = JUMPING
         
-        self.dino_duck = False
-        self.dino_run = True
-        self.dino_jump = False
+        self.capy_duck = False
+        self.capy_run = True
+        self.capy_jump = False
         
         self.step_index = 0
         self.jump_vel = self.JUMP_VEL
         self.image = self.run_img[0]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = self.X_POS
-        self.dino_rect.y = self.Y_POS
+        self.capy_rect = self.image.get_rect()
+        self.capy_rect.x = self.X_POS
+        self.capy_rect.y = self.Y_POS
         
     def update(self, userInput):
-        if self.dino_duck:
+        if self.capy_duck:
             self.duck()
-        if self.dino_run:
+        if self.capy_run:
             self.run()
-        if self.dino_jump:
+        if self.capy_jump:
             self.jump()
             
         if self.step_index >= 10:
             self.step_index = 0
             
-        if userInput[pygame.K_UP] and not self.dino_jump:
-            self.dino_duck = False
-            self.dino_run = False
-            self.dino_jump = True
-        elif userInput[pygame.K_DOWN] and not self.dino_jump:
-            self.dino_duck = True
-            self.dino_run = False
-            self.dino_jump = False
-        elif not (self.dino_jump or userInput[pygame.K_DOWN]):
-            self.dino_duck = False
-            self.dino_run = True
-            self.dino_jump = False
+        if userInput[pygame.K_UP] and not self.capy_jump:
+            self.capy_duck = False
+            self.capy_run = False
+            self.capy_jump = True
+        elif userInput[pygame.K_DOWN] and not self.capy_jump:
+            self.capy_duck = True
+            self.capy_run = False
+            self.capy_jump = False
+        elif not (self.capy_jump or userInput[pygame.K_DOWN]):
+            self.capy_duck = False
+            self.capy_run = True
+            self.capy_jump = False
             
     def duck(self):
         self.image = self.duck_img[self.step_index // 5]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = self.X_POS
-        self.dino_rect.y = self.Y_POS_DUCK
+        self.capy_rect = self.image.get_rect()
+        self.capy_rect.x = self.X_POS
+        self.capy_rect.y = self.Y_POS_DUCK
         self.step_index += 1
 
     def run(self):
         self.image = self.run_img[self.step_index // 5]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = self.X_POS
-        self.dino_rect.y = self.Y_POS
+        self.capy_rect = self.image.get_rect()
+        self.capy_rect.x = self.X_POS
+        self.capy_rect.y = self.Y_POS
         self.step_index += 1
     
     def jump(self):
         self.image = self.jump_img
-        if self.dino_jump:
-            self.dino_rect.y -= self.jump_vel * 4
+        if self.capy_jump:
+            self.capy_rect.y -= self.jump_vel * 4
             self.jump_vel -= 0.8
         if self.jump_vel < - self.JUMP_VEL:
-            self.dino_jump = False
+            self.capy_jump = False
             self.jump_vel = self.JUMP_VEL
     
     def draw(self, SCREEN):
-        SCREEN.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+        SCREEN.blit(self.image, (self.capy_rect.x, self.capy_rect.y))
 
 class Cloud:
     def __init__(self):
@@ -180,7 +180,7 @@ def main():
     global game_speed, x_pos_bg, y_pos_bg, points, obstacles
     run = True
     clock = pygame.time.Clock()
-    player = Dinosaur()
+    player = Capybara()
     cloud = Cloud()
     game_speed = 14
     x_pos_bg = 0
@@ -233,8 +233,8 @@ def main():
         for obstacle in obstacles:
             obstacle.draw(SCREEN)
             obstacle.update()
-            if player.dino_rect.colliderect(obstacle.rect):
-                # pygame.draw.rect(SCREEN, (255, 0, 0), player.dino_rect, 2) #mostrar hitbox cuando choca con obstaculo
+            if player.capy_rect.colliderect(obstacle.rect):
+                # pygame.draw.rect(SCREEN, (255, 0, 0), player.capy_rect, 2) #mostrar hitbox cuando choca con obstaculo
                 pygame.time.delay(2000)
                 death_count += 1
                 menu(death_count)
