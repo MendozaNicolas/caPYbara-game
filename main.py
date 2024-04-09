c6b3b017a711f140050833f22e6dbdfe07061cc0
MendozaNicolas <nicolas.mendozaion@gmail.com>
2024-04-09 02:54:44 -0300

    agregue menu y funcion gameover

--- main.py
+++ main.py
@@ -173,6 +173,7 @@ def main():
     points = 0
     font = pygame.font.Font('freesansbold.ttf', 20)
     obstacles = []
+    death_count = 0
     
     def score():
         global points, game_speed
@@ -217,7 +218,10 @@ def main():
             obstacle.draw(SCREEN)
             obstacle.update()
             if player.dino_rect.colliderect(obstacle.rect):
-                pygame.draw.rect(SCREEN, (255, 0, 0), player.dino_rect, 2)
+                # pygame.draw.rect(SCREEN, (255, 0, 0), player.dino_rect, 2) #mostrar hitbox cuando choca con obstaculo
+                pygame.time.delay(2000)
+                death_count += 1
+                menu(death_count)
         
         cloud.draw(SCREEN)
         cloud.update()
@@ -229,4 +233,32 @@ def main():
         clock.tick(30)
         pygame.display.update()
 
-main()
+
+
+def menu(death_count):
+    global points
+    run = True
+    while run:
+        SCREEN.fill((255, 255, 255))
+        font = pygame.font.Font('freesansbold.ttf', 20)
+        
+        if death_count == 0:
+            text = font.render ("Presiona cualquier tecla para empezar", True, (0, 0, 0))
+        elif death_count > 0:
+            text = font.render ("Presiona cualquier tecla para volver a empezar", True, (0, 0, 0))
+            score = font.render("Tus puntos: " + str(points), True, (0, 0, 0))
+            scoreRect = score.get_rect()
+            scoreRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50)
+            SCREEN.blit(score, scoreRect)
+        textRect = text.get_rect()
+        textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+        SCREEN.blit(text, textRect)
+        SCREEN.blit(RUNNING[0], (SCREEN_WIDTH // 2 - 20, SCREEN_HEIGHT // 2 - 140))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                main()
+
+menu(death_count=0)
